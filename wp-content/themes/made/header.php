a0023134feb66cefdcb210ffcc90a48c43c42c4d
matthieuP <matthieu.posnic@hotmail.fr>
2014-11-04 22:36:56 +0100

    ajout

--- wp-content/themes/made/header.php
+++ wp-content/themes/made/header.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
- title><?php bloginfo('name') ?><?php if ( is_404() ) : ?> &raquo; <?php _e('Not Found') ?><?php elseif ( is_home() ) : ?> &raquo; <?php bloginfo('description') ?><?php else : ?><?php wp_title() ?><?php endif ?></title>
  
-
-    <!-- leave this for stats -->
  
-    <?php wp_get_archives('type=monthly&format=link'); ?>
-    <?php //comments_popup_script(); // off by default ?>
-    <?php wp_head(); ?>
 
   <body>
--- wp-content/themes/made/index.php
+++ wp-content/themes/made/index.php
@@ -8,17 +8,11 @@
 
         <meta name="viewport" content="width=device-width">
 
-        <title><?php wp_title( '|', true, 'right' ); ?></title>
-
         <link rel="profile" href="http://gmpg.org/xfn/11">
 
         <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css" type="text/css" media="screen" />
 
-        <!--[if lt IE 9]>
-10
-        <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
-11
-        <![endif]-->
+        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css" type="text/css" media="screen" />
 
         <?php wp_head(); ?>
 
@@ -30,24 +24,19 @@
     while (have_posts()) : the_post(); 
     $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
 
-      
     <div  class="etudiants" style="background:url(<?php echo $featuredImage[0]; ?>) no-repeat;
      height:<?php echo $featuredImage[2]; ?>; 
      width:<?php echo $featuredImage[1]; ?>
      no-repeat no-repeat center center fixed; 
      -webkit-background-size : cover ; 
-  -moz-background-size : cover ; 
-  -o-background-size : cover ; 
-  background-size : cover ;">
-      <?php the_content(); ?></div>
-   
+    -moz-background-size : cover ; 
+    -o-background-size : cover ; 
+    background-size : cover ;">
+      <?php the_content(); ?>
+    </div>
 
 <?php endwhile; wp_reset_query(); ?>
 
-    	<div class = "etudiants"style="">
-     
-        
-      </div>
 
 
 <?php get_footer(); ?>
