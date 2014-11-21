2ccae18d3d9e0ca7e5f4149697bf0e6c8ceb819d
matthieuP <matthieu.posnic@hotmail.fr>
2014-11-22 00:41:58 +0100

    ligne 1 terminée

--- wp-content/themes/made/home.php
+++ wp-content/themes/made/home.php
@@ -1,19 +1,17 @@
 <?php get_header(); ?>
 
 	<div class="ligneune">
-		<?php $my_query = new WP_Query(array('post_type' => 'ligne1', 'orderby' => 'title', 'order'=>'ASC', 'posts_per_page' => '4')); ?>
-			<?php while ($my_query->have_posts()) : $my_query->the_post();?>
+		<?php $my_query = new WP_Query(array('post_type' => 'ligne1', 'orderby' => 'date', 'order'=>'DESC', 'posts_per_page' => '4')); ?>
 			<div class="container-fluid">
 				<div class="row">
+		<?php while ($my_query->have_posts()) : $my_query->the_post();?>
 					<div class="col-md-3">
-						<img src='<?php the_field('image') ?>' <?php the_field('ligne1') ?>>
+						<h1><?php the_field('ligne1') ?></h1>
+						<img src='<?php the_field('image') ?>'>
 					</div>
-					<div class="col-md-3">
-						<img src='<?php the_field('image') ?>' <?php the_field('ligne1') ?>>
-					</div>
+		<?php endwhile; ?>
 				</div>
 			</div>
-			<?php endwhile; ?>
 	</div>    
 
 	<div class="lignedeux">
