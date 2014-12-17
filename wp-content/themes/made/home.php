6d559b31740648199ec46ad6d450c157905ddedb
matthieuP <matthieu.posnic@hotmail.fr>
2014-12-17 23:52:16 +0100

    bug git pour amine et laura

--- wp-content/themes/made/home.php
+++ wp-content/themes/made/home.php
@@ -1,17 +1,18 @@
 <?php get_header(); ?>
 
-	<div class="ligneune">
-		<?php $my_query = new WP_Query(array('post_type' => 'addarticle', 'orderby' => 'date', 'order'=>'DESC', 'posts_per_page' => '20')); ?>
+	<?php $my_query = new WP_Query(array('post_type' => 'addarticle', 'orderby' => 'date', 'order'=>'DESC', 'posts_per_page' => '5')); ?>
 			<div class="container-fluid">
 				<div class="row">
-		<?php while ($my_query->have_posts()) : $my_query->the_post();?>
-					<div class="col-md-3">
-						<h1><?php the_field('addtext') ?></h1>
-						<img src='<?php the_field('addimage') ?>'>
-					</div>
-		<?php endwhile; ?>
+					<?php while ($my_query->have_posts()) : $my_query->the_post();?>
+						<div class="col-md-3">
+							<h1><?php the_field('addtext') ?></h1>
+							<img src='<?php the_field('addimage') ?>'>
+						</div>
+					<?php endwhile; ?>
+					
 				</div>
 			</div>
-	</div>
+
+
 
 <?php get_footer(); ?>
