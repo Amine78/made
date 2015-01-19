b2fde049ea48198f1a3be5c6c1314051fd81edf1
Amine78 <amine.rezine@gmail.com>
2015-01-19 10:47:20 +0100

    plugin

---
+++ wp-content/plugins/flagallery-skins/cube_jn/cube_jn.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Skin Name: Cube 3D 
+Version: 3.1
+uid: cube
+Skin URI: http://photogallerycreator.com/grand-flagallery/3d-cube-gallery/
+Description: <strong>Compatible:</strong><li>HitCounter.</li><li>Alternate jQuery gallery for iPhone / iPad devices without flash player.</li><li>SEO optimized and support feed RSS2.</li><li>GRAND Pages - FullWindow Mode.</li><li>FullScreen Mode (On/Off)</li><li>Thumbnails Hints</li><br /><strong style="color: red;">Requirements - GRAND FlAGallery v2.0 or higher.</strong>
+Author: PGC
+Author URI: http://PhotoGalleryCreator.com
+*/
+?>
