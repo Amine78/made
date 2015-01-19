5005eb6ca5c6e27e6067bd14e641c17787c1c203
matthieuP <matthieu.posnic@hotmail.fr>
2015-01-19 11:32:16 +0100

    raz

--- wp-content/themes/gamencake/function.php
+++
@@ -1,5 +0,0 @@
-
-/*Enlever le titre*/
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 30 );/**/
-?>
