d9ab1ff4595199a99bb1f58587d90f14f867e9a4
matthieuP <matthieu.posnic@hotmail.fr>
2015-01-20 12:21:35 +0100

    reset

--- wp-content/themes/made/index.php
+++ wp-content/themes/made/index.php
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8"/>
-<title>Made In IIM </title>
 
-<!-- The Swipebox plugin -->
-<link href="<?php bloginfo('template_directory'); ?>/assets/swipebox/swipebox.css" rel="stylesheet" />
-
-<!-- The main CSS file -->
-<link href="<?php bloginfo('template_directory'); ?>/assets/css/style.css" rel="stylesheet" />
-
-<!--[if lt IE 9]>
-			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-</head>
-
-<body>
-<?php
- $my_query = new WP_Query(array('post_type' => 'ligne1', 'orderby' => 'title', 'order'=>'ASC', 'posts_per_page' => '4'));?> 			<?php while ($my_query->have_posts()) : $my_query->the_post();?>					
-						<a class="swipebox show" src='<?php the_field('ligne1')?>'
-
-	<?php	 endwhile; ?>
-<div id="gallery"></div>
-
-<!-- JavaScript Includes --> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
-<script src="<?php bloginfo('template_directory'); ?>/assets/swipebox/jquery.swipebox.min.js"></script> 
-<script src="/assets/js/jquery.loadImage.js"></script> 
-<script src="<?php bloginfo('template_directory'); ?>/assets/js/script.js"></script>
-</body>
-</html>
-=======
 <?php get_header(); ?>
 
 <?php get_footer(); ?>
->>>>>>> 5d3aad6e0bdbdb8aea3bc59c908d582b31c13df7
