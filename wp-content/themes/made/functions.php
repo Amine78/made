9080f5b6523b1c234ff1989118fb8afb2711bca3
matthieuP <matthieu.posnic@hotmail.fr>
2015-01-20 12:16:20 +0100

    reset

--- wp-content/themes/made/functions.php
+++ wp-content/themes/made/functions.php
@@ -13,13 +13,13 @@ function cpt_init() {
         'add_new_item' => 'Ajouter une nouvelle photo pour l&apos;article',
         'edit_item' => 'Editer une photo sur l&apos;article',
         'new_item' => 'Nouvelle photo sur l&apos;article',
-        'all_items' => 'Tout les articles',
+        'all_items' => 'Tous les articles',
         'view_item' => 'Voir sur l&apos;article',
         'search_items' => 'Chercher photo sur l&apos;article',
         'not_found' =>  'Aucune photo trouvée sur l&apos;article',
         'not_found_in_trash' => 'Aucun article trouvée dans la corbeille',
         'parent_item_colon' => '',
-        'menu_name' => 'Ajouter un article'
+        'menu_name' => 'Ajouter un article sur la première ligne'
     );
     $args = array(
         'labels' => $labels,
@@ -36,7 +36,105 @@ function cpt_init() {
         'supports' => array( 'title', 'thumbnail')
     );
 
-    register_post_type('addarticle', $args );
+    register_post_type('addarticlefirst', $args );
+
+
+    $labels = array(
+        'name' => 'Ajouter un article',
+        'singular_name' => 'lignehaut',
+        'add_new' => 'Ajouter un article',
+        'add_new_item' => 'Ajouter une nouvelle photo pour l&apos;article',
+        'edit_item' => 'Editer une photo sur l&apos;article',
+        'new_item' => 'Nouvelle photo sur l&apos;article',
+        'all_items' => 'Tous les articles',
+        'view_item' => 'Voir sur l&apos;article',
+        'search_items' => 'Chercher photo sur l&apos;article',
+        'not_found' =>  'Aucune photo trouvée sur l&apos;article',
+        'not_found_in_trash' => 'Aucun article trouvée dans la corbeille',
+        'parent_item_colon' => '',
+        'menu_name' => 'Ajouter un article sur la deuxième ligne'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'addarticle' ),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array( 'title', 'thumbnail')
+    );
+
+    register_post_type('addarticlesecond', $args );
+
+    $labels = array(
+        'name' => 'Ajouter un article',
+        'singular_name' => 'lignehaut',
+        'add_new' => 'Ajouter un article',
+        'add_new_item' => 'Ajouter une nouvelle photo pour l&apos;article',
+        'edit_item' => 'Editer une photo sur l&apos;article',
+        'new_item' => 'Nouvelle photo sur l&apos;article',
+        'all_items' => 'Tous les articles',
+        'view_item' => 'Voir sur l&apos;article',
+        'search_items' => 'Chercher photo sur l&apos;article',
+        'not_found' =>  'Aucune photo trouvée sur l&apos;article',
+        'not_found_in_trash' => 'Aucun article trouvée dans la corbeille',
+        'parent_item_colon' => '',
+        'menu_name' => 'Ajouter un article sur la troisième ligne'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'addarticle' ),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array( 'title', 'thumbnail')
+    );
+
+    register_post_type('addarticlethird', $args );
+
+    $labels = array(
+        'name' => 'Ajouter un article',
+        'singular_name' => 'lignehaut',
+        'add_new' => 'Ajouter un article',
+        'add_new_item' => 'Ajouter une nouvelle photo pour l&apos;article',
+        'edit_item' => 'Editer une photo sur l&apos;article',
+        'new_item' => 'Nouvelle photo sur l&apos;article',
+        'all_items' => 'Tous les articles',
+        'view_item' => 'Voir sur l&apos;article',
+        'search_items' => 'Chercher photo sur l&apos;article',
+        'not_found' =>  'Aucune photo trouvée sur l&apos;article',
+        'not_found_in_trash' => 'Aucun article trouvée dans la corbeille',
+        'parent_item_colon' => '',
+        'menu_name' => 'Ajouter un article sur la quatrième ligne'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'addarticle' ),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array( 'title', 'thumbnail')
+    );
+
+    register_post_type('addarticlefourth', $args );
 }
+
 
 add_action('init', 'cpt_init');
--- wp-content/themes/made/header.php
+++ wp-content/themes/made/header.php
@@ -9,10 +9,11 @@
         <link rel="profile" href="http://gmpg.org/xfn/11">
 
         <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css" type="text/css" media="screen" />
-		
-		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js" type="text/css" media="screen" />
 
         <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css" type="text/css" media="screen" />
+		
+		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js" type="text/js" media="screen" />
+
 
    </head>
 
